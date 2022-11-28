ee8567b28ac01dc73dad8a1b53cdf1480d8fe901
vanex97 <vanex97@gmail.com>
2022-11-28 18:40:52 +0200

    Update: meta description and title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
-@section('title', 'Home')
+@section('title', 'DeSo BlockChain Explorer')
 
 @section('content')
     <div class="main-search-home">
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="ScanDeso is a Block Explorer and Analytics Platform for DeSo – The Decentralized Social Blockchain"/>
+    <meta name="robots" content="follow, index"/>
     <link rel="icon" type="image/x-icon" href="/favicon.png">
     <link rel="preload" href="/css/bootstrap-cyborg.min.css" as="style"/>
     <link rel="preload" href="/css/bootstrap.min.css" as="style"/>
